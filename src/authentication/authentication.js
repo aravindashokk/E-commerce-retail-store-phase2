f6bb45e5c3375d40f12061629376ae9f89ef894d
Neeharika Kosana <kosananeeharika@gmail.com>
2022-10-03 15:00:23 -0500

    added super admin and school admin to the login page

--- src/authentication/authentication.js
+++ src/authentication/authentication.js
@@ -2,9 +2,12 @@
 function login(element) {
     let userType = element.value;
     localStorage.setItem('userType', userType);
-    if (userType === 'manager' || userType === 'admin') {
-        window.location.href = '../Manager/manager.html';
-    } else if (userType === 'user') {
+    if (userType === 'Super Admin' || userType === 'admin') {
+        window.location.href = '../Super Admin/Super Admin.html';
+    }
+    else if (userType === 'School Admin') {
+        window.location.href = '../School Admin/School Admin.html'; }
+    else if (userType === 'user') {
         window.location.href = '../Users/users.html';
     } else if (userType === 'visitor') {
         window.location.href = '../Visitor/visitor.html'
